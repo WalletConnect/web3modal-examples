f18b10e1e9cc65c4298bd43915caa001f0e93893
Ilja <idaderko@gmail.com>
2023-06-26 14:43:51 +0300

    Update examples to wcm 2.5.4

--- walletconnect-modal-auth-html-cdn/main.js
+++ walletconnect-modal-auth-html-cdn/main.js
@@ -1,4 +1,4 @@
-import { WalletConnectModalAuth } from "https://unpkg.com/@walletconnect/modal-auth-html@2.5.3";
+import { WalletConnectModalAuth } from "https://unpkg.com/@walletconnect/modal-auth-html@2.5.4";
 
 // 1. Define ui elements
 const connectButton = document.getElementById("connect-button");
